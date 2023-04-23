ce986e38553a88f9178955579d26fa4bee94176d
Dng16 <dongyang0016@gmail.com>
2023-04-23 14:16:45 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const arrrowLeft = document.querySelector('.btn-arrow-left');
+const arrrowRight= document.querySelector('.btn-arrow-right');
+const slide2 = document.querySelector('.slide-2');
+const slide1 = document.querySelector('.slide-1');
+
+// console.log(arrrowLeft,arrrowRight);
+
+arrrowRight.addEventListener('click', () =>{
+  slide1.classList.add('hidden')
+  slide2.classList.remove('hidden')
+  slide2.classList.add('show')
+
+});
+arrrowLeft.addEventListener('click', () =>{
+
+  slide1.classList.remove('hidden')
+  slide2.classList.add('hidden')
+  slide2.classList.remove('show')
+});
